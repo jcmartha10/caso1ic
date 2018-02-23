b981df5b8dca808623a481bf374e34ac0bd8d8a1
JuanK <jc.martha10@uniandes.edu.co>
2018-02-23 13:44:10 -0500

    Buffer Implementado

--- caso1/src/logica/Buffer.java
+++ caso1/src/logica/Buffer.java
@@ -1,5 +1,46 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Buffer {
 
+	private ArrayList<Mensaje> mensajes;
+	private int tamanio;
+	private Object lleno;
+	private Object vacio;
+	
+	public Buffer(int pTamanio) {
+		tamanio = pTamanio;
+		mensajes = new ArrayList<Mensaje>();
+		lleno = new Object();
+		vacio = new Object();
+	}
+	
+	public void encolar(Mensaje m) {
+		synchronized (lleno) {
+			while (mensajes.size() == tamanio) {
+				try {
+					lleno.wait();
+				} catch (InterruptedException e) {}
+			}
+		}
+		
+		synchronized(this){mensajes.add(m);}
+		synchronized(vacio) {vacio.notify();}
+	}
+	
+	public Mensaje desencolar() {
+		synchronized (vacio) {
+			while (mensajes.size() == 0) {
+				try {
+					vacio.wait();
+				} catch (InterruptedException e) {}
+			}
+		}
+		
+		Mensaje temp;
+		synchronized(this){temp = mensajes.remove(0);}
+		synchronized(lleno) {lleno.notify();}
+		return temp;
+	}
 }
--- caso1/src/logica/Cliente.java
+++ caso1/src/logica/Cliente.java
@@ -1,5 +1,9 @@
 package logica;
 
 public class Cliente {
-
+	private Buffer buffer;
+	
+	public Cliente(Buffer pBuffer) {
+		buffer = pBuffer;
+	}
 }
