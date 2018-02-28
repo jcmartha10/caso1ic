c917f992b0bd24a690c01fad21a743e8d64638cd
nsanabria10 <nicolassana@gmail.com>
2018-02-28 09:38:15 -0500

    1
    Implementación Mensaje

--- caso1/src/logica/Mensaje.java
+++ caso1/src/logica/Mensaje.java
@@ -1,5 +1,27 @@
 package logica;
 
 public class Mensaje {
-
+	private int idCliente;
+	private int consulta;
+	private int respuesta;
+	
+	public Mensaje(int id, int consulta) {
+		this.idCliente = id;
+		this.consulta = consulta;
+		this.respuesta = -1;
+	}
+	public int getConsulta() {
+		return consulta;
+	}
+	public void setConsulta(int consulta) {
+		this.consulta = consulta;
+	}
+	public int getRespuesta() {
+		return respuesta;
+	}
+	public void setRespuesta(int respuesta) {
+		this.respuesta = respuesta;
+	}
+	
+	
 }
